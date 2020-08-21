9efc43ee3d4395f7f85948f962d7e8b9a4b00ea2
ramesht007 <shre611shukla@gmail.com>
2020-08-21 21:34:51 +0530

    # kfolds

---
+++ src/create_folds.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import model_selection
+
+
+if __name__ == "__main__":
+    df = pd.read.csv("input/train.csv")
+    df["kfold"] = -1
+
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    kf = model_selection.StratifiedkFold(n_splits=5, shuffle-False, random_state=42)
+
+
+    for fold(train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
+        print(len(train_idx), len(val_idx))
+        df.loc[val_idx, 'kfold'] = fold
+
+
+    df.to_csv("input/train_folds.csv", index-False)
