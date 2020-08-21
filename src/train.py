fe1cbc91c4d6346959eb8f9fd507a1923a30b9cd
ramesht007 <shre611shukla@gmail.com>
2020-08-21 21:59:56 +0530

    # added_encoder

--- src/train.py
+++ src/train.py
@@ -1,8 +1,9 @@
+import os
 import pandas as pd
 from sklearn import preprocessing 
 
-TRAINING_DATA = None
-FOLD = None
+TRAINING_DATA = os.environ.get("TRAINING_DATA")
+FOLD = os.environ.get("FOLD")
 
 FOLD_MAPPING = {
     0: [1, 2, 3, 4],
@@ -17,4 +18,19 @@ if __name__ == "__main__":
     train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
     valid_df = df[df.kfold==FOLD]
 
-    
+    ytrain = train_df.target.values
+    yvalid = valid_df.target.values
+
+    train_df = train_df.drop(["id", "target", "kfold"], axis=1)
+    valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
+
+
+    valid_df = valid_df[train_df.columns]    
+
+    label_encoders = []
+    for c in train_df.columns:
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
+        valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
+        label_encoders.append((c, lbl))
